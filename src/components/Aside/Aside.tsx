4b25e2adfd7aa32048863c7da4ee29ae821fdce9
Christian Fowler <cfowlerdev@gmail.com>
2023-03-11 13:46:30 +0800

    Updated main component colours

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -5,5 +5,9 @@ interface IProps {
 }
 
 export const Aside: React.FC<IProps> = ({ children }) => {
-  return <aside className="fixed top-16 left-0 h-screen w-16 bg-yellow-500">{children}</aside>;
+  return (
+    <aside className="fixed top-16 left-0 h-screen w-16 bg-paper text-paper-text dark:bg-darkmode-paper dark:text-darkmode-paper-text">
+      {children}
+    </aside>
+  );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,9 +6,9 @@ interface IProps {
 
 export const Navbar: React.FC<IProps> = ({ children }) => {
   return (
-    <nav className="fixed top-0 left-0 z-20 h-16 w-full bg-blue-200">
+    <nav className="fixed top-0 left-0 z-20 h-16 w-full bg-paper text-paper-text dark:bg-darkmode-paper dark:text-darkmode-paper-text">
       <div className="container flex">
-        <div className="flex h-16 w-16 items-center bg-primary">Logo Here</div>
+        <div className="flex h-16 w-16 items-center bg-paper-darker dark:bg-darkmode-paper-darker">Logo Here</div>
         <div className="flex items-center px-4">Dashboard</div>
         <div className="flex items-center justify-center px-4">Menu</div>
         <div className="absolute inset-y-0 right-0 flex items-center pr-4">Right</div>
